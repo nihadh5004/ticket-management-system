633bde8a78f3743535288c994ee46225ff2b631e
Nihadh <nihadhfizz6@gmail.com>
2025-01-15 13:49:07 +0530

    added password validation

--- ticket-frontend/src/app/components/Auth/Signup.tsx
+++ ticket-frontend/src/app/components/Auth/Signup.tsx
@@ -96,7 +96,12 @@ export default function Signup() {
               className={`mt-1 block w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 text-sm ${
                 errors.password ? 'border-red-500' : 'border-gray-300'
               }`}
-              {...register('password', { required: 'Password is required' })}
+              {...register('password', { required: 'Password is required' ,
+                minLength: {
+                  value: 4,
+                  message: 'Password must be at least 4 characters',
+                }}
+              )}
             />
             {errors.password && (
               <p className="text-sm text-red-500 mt-1">{String(errors.password.message)}</p>
